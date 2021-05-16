fa8c61c82a843e13175af5e681c0e52ae351f804
Saketh-77 <50708056+Saketh-77@users.noreply.github.com>
2021-05-16 20:54:25 +0530

    Updated Angles

--- resources/flight_simulator/main.cpp
+++ resources/flight_simulator/main.cpp
@@ -2,9 +2,12 @@
 #include<GL/glu.h>
 #include<GL/gl.h>
 #include<bits/stdc++.h>
+#include<fstream>
+#include<iostream>
+#include<stdlib.h>
 #include "trajectory.h"
 #include<cmath>
-#define PI 3.1415926535898
+#define PI 3.14159265358979323846
 #define Cos(th) cos(PI/180*(th))
 #define Sin(th) sin(PI/180*(th))
 /*  D degrees of rotation */
@@ -23,13 +26,6 @@ Trajectory trajectory;
 // Points
 Points p,p1,p2;
 
-// Model vertices
-Points v1(0.0, 1.0, 0.0),
-       v2(0.0,-1.0, 0.0),
-       v3(3.0,-1.0, 0.0),
-       v4(4.0,0.0,0.0),
-       v5(3.0, 1.0, 0.0);
-
 // Trajectory related variables
 bool isInitial = true, newPath = false;
 double distanceBetweenPoints = 0.0;
@@ -41,33 +37,47 @@ vector<double> directionVector;
 
 //initial position of x
 double x_position = 0.0, y_position = 0.0, z_position = -20.0;
-double angle_x = 60.0, angle_y = 0.0, angle_z = -90.0,rotAngle = 90.0;
+double angle_x = -60.0, angle_y = 180.0, angle_z = -90.0;
 double x_direction = 0, y_direction = 0, z_direction = 0;
 int N = 0;
 
-void display();
+void displayAnimationWindow();
 void reshape(int, int);
 void timer(int);
 
 
 // Under development
+void updateAngle();
 void updateAngles(double Ax, double Ay, double Az){
 
 
     double A = sqrt(pow(Ax,2)+pow(Ay,2)+pow(Az,2));
 
-    angle_x = angle_x+acos(Ax/A)*10;
-    angle_y = angle_y+acos(Ay/A)*5;
-    angle_z = angle_z-acos(Az/A)*20;
+    double currentAngleX = acos(Ax/A)*(180/PI);
+    double currentAngleY = acos(Ay/A)*(180/PI);
+    double currentAngleZ = acos(Az/A)*(180/PI);
 
-    cout<<"The angles are :"<<angle_x<<" "<<angle_y<<" "<<angle_z<<endl;
+    angle_x = currentAngleX;
+    angle_y = currentAngleY;
+    angle_z = currentAngleZ;
+
+    // cout<<"The angles are :"<<angle_x<<" "<<angle_y<<" "<<angle_z<<endl;
 }
 
-void updateAngle(Points p, Points q) {
+vector<Points> generateArc(Points center, Points start, Points end, double radius) {
+    double tx = start.x;
 
+    //h,k = center of the circle
+    vector<Points> res;
 
+    while(abs(end.x-tx)>0.1) {
+        double ty = center.y + sqrt(pow(radius,2) - pow(tx - center.x,2));
+        res.push_back(Points(tx, ty, -20));
+        tx-=0.099999999;
+    }
+
+    return res;
 }
-
 
 // Load the trajectories from a file
 void loadTrajectory(){
@@ -77,11 +87,20 @@ void loadTrajectory(){
 //    cin>>n;
 
     vector<Points> points {
-    //Points(0,0,-20),Points(15,1,-45),Points(25,3,-55),Points(2,5,-25),Points(1,3,-20),Points(-5,5,-30)
-    Points(-10.7,-11.32,-20),Points(-2,-11.32,-20),Points(6,-7,-20),Points(8,0,-20),Points(6,10,-35),Points(3,13,-35),Points(-3,8,-25),Points(-4,6,-24),Points(-7,2,-25),Points(-10.5,-10,-20)
-    //Points(-12,-13,-20),Points(-5,-13,-20),Points(5,-7,-20),Points(-4,-4,-20),Points(-6,-8,-20),Points(-10,-11,-20)
 
+    Points(-10.7,-11.32,-20),Points(-2,-11.32,-20),Points(6,-7,-20),Points(7.9,1,-20),/*The arc points are inserted*/Points(2,6,-20),Points(-3,8,-25),Points(-4,6,-24),Points(-7,2,-25),Points(-10.7,-11.32,-20)
     };
+
+    //arc points
+    double radius = 6;
+    Points center = Points(2,0,-20), startPoint = points[3], endPoint = points[4];
+
+    vector<Points> arcArr = generateArc(center, startPoint, endPoint, radius);
+
+    auto it = points.begin();
+    auto itArc = arcArr.begin();
+    points.insert(it+4, arcArr.begin()+1, arcArr.end());
+
 //    Points point;
 //    for(int i=0;i<n;i++){
 //        cout<<"Enter x, y, z for point "<<(i+1)<<endl;
@@ -105,28 +124,71 @@ void init(){
     glClearColor(0.7,0.7,0.7,1.0);
     glEnable(GL_DEPTH_TEST);
 }
+void displayEmptyScreen(){
+    glClearColor(0.7,0.7,0.7,1.0);
+    glFlush();
+}
+
+void StartMenu(int n){
+    switch(n)
+    {
+        case 1:
+            loadTrajectory();
+
+
+            glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+            glutInitWindowPosition(200,100);
+            glutInitWindowSize(1920,1200);
+
+            glutCreateWindow("Flight");
+            glutDisplayFunc(displayAnimationWindow);
+            glutReshapeFunc(reshape);
+            glutTimerFunc(0, timer, 0);
+            init();
+        break;
+
+        case 2:
+            break;
+    }
+
+    glutPostRedisplay();
+}
 
 int main(int argc, char** argv) {
-
-    loadTrajectory();
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
     glutInitWindowPosition(200,100);
     glutInitWindowSize(1920,1200);
 
-    glutCreateWindow("Flight");
+    glutCreateWindow("Menu");
 
+
+
+    glutCreateMenu(StartMenu);
+    glutAddMenuEntry("Start",1);
+    glutAddMenuEntry("Exit",0);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
+
+    glutDisplayFunc(displayEmptyScreen);
     //callback functions
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutTimerFunc(0, timer, 0);
-    init();
-
     glutMainLoop();
 }
 
-void display() {
+void renderBitMap(double x, double y, double z, void *font, char *title){
+    glRasterPos2f(x,y);
+    for(char *c=title; *c!='\0';c++){
+        glColor3f(0,0,0);
+        glutBitmapCharacter(font, *c);
+    }
+}
+
+void displayAnimationWindow() {
+
+    // Text
+    char textBuffer[100] = {0};
+    sprintf_s(textBuffer,"Amrita");
+    renderBitMap(800,800,-20,GLUT_BITMAP_HELVETICA_18,textBuffer);
 
     GLfloat Pos[] = {0,3,0,1} ;
     GLfloat Col[] = {1,0,0,1} ;
@@ -143,9 +205,9 @@ void display() {
     glTranslatef(x_position,y_position,z_position);
     //gluLookAt(0,1,3,0,0,0,0,1,0) ;
 
-    glRotatef(angle_x,1.0,0.0,0.0);
-    glRotatef(angle_y,0.0,1.0,0.0);
-    glRotatef(angle_z,0.0,0.0,1.0);
+    glRotatef(angle_x,p2.x,0.0,0.0);
+    glRotatef(angle_y,0.0,p2.y,0.0);
+    glRotatef(angle_z,0.0,0.0,p2.z);
 
     glShadeModel(GL_SMOOTH);
 
@@ -216,18 +278,10 @@ void reshape(int w, int h) {
 bool checkPrecision(double point_i, double point_f){
 
     bool result=false;
-    double difference = (point_i-point_f);
+    double difference = abs(point_i-point_f);
     return (difference<=0.1);
 
 }
-
-bool findMaxDifference(double a, double b){
-    return (a<b);
-}
-
-/*
-double maxDifference = std::max({p2.x-x_position, p2.y-y_position, p2.z-z_position},findMaxDifference);
-*/
 
 void timer(int) {
 
@@ -241,8 +295,8 @@ void timer(int) {
     if(isInitial || newPath){
         if(trajectory.cpi==N) {
 
-            angle_x = 60.0;
-            angle_y = 0.0;
+            angle_x = -60.0;
+            angle_y = 180.0;
             angle_z = -90.0;
             //stop when the plane complete one cycle on the trajectory.
             return;
@@ -263,6 +317,8 @@ void timer(int) {
         y_direction = (p2.y-p1.y)/distanceBetweenPoints;
         z_direction = (p2.z-p1.z)/distanceBetweenPoints;
 
+        // Update angle
+
         isInitial = false;
         newPath = false;
 
@@ -275,7 +331,7 @@ void timer(int) {
     if((frameindex<=numberOfFrames) && !(checkPrecision(p2.x,x_position) && checkPrecision(p2.y,y_position) && checkPrecision(p2.z,z_position)))
     {
 
-        cout<<"\n Difference: x-axis: "<<p2.x-x_position<<" y-axis: "<<p2.y-y_position<<" z-axis: "<<p2.z-z_position<<"\n"<<endl;
+        cout<<"Difference: x-axis: "<<p2.x-x_position<<" y-axis: "<<p2.y-y_position<<" z-axis: "<<p2.z-z_position<<endl;
         cout<<"Current Position: ("<<x_position<<", "<<y_position<<", "<<z_position<<")"<<endl;
 
         double xRate = (x_direction*eachFrameLength*frameindex),
@@ -286,7 +342,7 @@ void timer(int) {
         y_position = y_position + yRate;
         z_position = z_position + zRate;
 
-        frameindex = frameindex + 0.0004;
+        frameindex = frameindex + 0.0001;
 
         cout<<"Next Position: ("<<x_position<<", "<<y_position<<", "<<z_position<<")"<<endl;
 
@@ -297,18 +353,44 @@ void timer(int) {
         y_position = p2.y;
         z_position = p2.z;
 
-        //
-        updateAngles(x_direction, y_direction, z_direction);
+        // Update angles
+        //updateAngles(x_direction, y_direction, z_direction);
+        //angleBetweenVectors(p1,p2);
+        updateAngle();
 
         trajectory.cpi+=1;
         isInitial = false;
         newPath = true;
 
-        //frameindex = 0.0;
-
         cout<<"-----PATH END-----"<<endl;
 
     }
-
 }
 
+void updateAngle(){
+    if(trajectory.cpi == 0){
+        angle_x = -50;
+        angle_y = 190;
+        angle_z = -45;
+    }else if(trajectory.cpi == 1){
+        angle_x = -50;
+        angle_y = 130;
+        angle_z = -30;
+    }else if(trajectory.cpi == 2){
+        angle_x = -50;
+        angle_y = 130;
+        angle_z = -30;
+    }else if(trajectory.cpi > 2 && trajectory.cpi <= N-6){
+        angle_x += -5;
+        angle_y = -180;
+        angle_z = 90;
+    }else if(trajectory.cpi == N-5 || trajectory.cpi == N-4){
+        angle_x = 40;
+        angle_y = 70;
+        angle_z = 200;
+    }else{
+        angle_x = 40;
+        angle_y = 30;
+        angle_z = 180;
+    }
+}
